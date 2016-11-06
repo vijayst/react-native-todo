98aabc52fe82713ae01e5e49c1de4788fcd11617
vijayst <vijayst@gmail.com>
2016-11-06 20:41:39 +0530

    toolbar android.

--- todo/index.android.js
+++ todo/index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,43 +5,57 @@ import {
   Text,
   View
 } from 'react-native';
+import ToolbarAndroid from 'ToolbarAndroid';
 
-export default class todo extends Component {
+export default class Todo extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+    this.handleActionSelected = this.handleActionSelected.bind(this);
+  }
+
+  handleActionSelected(position) {
+    this.setState({
+      actionText: actions[position].title,
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <ToolbarAndroid
+          actions={actions}
+          onActionSelected={this.handleActionSelected}
+          style={styles.toolbar}
+          title={this.state.actionText}
+        />
       </View>
     );
   }
 }
 
+const actions = [
+  {
+    title: 'Todo',
+    show: 'always'
+  },
+  {
+    title: 'Add',
+    show: 'always'
+  },
+];
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  toolbar: {
+    backgroundColor: '#e9eaed',
+    height: 56,
   },
 });
 
-AppRegistry.registerComponent('todo', () => todo);
+AppRegistry.registerComponent('todo', () => Todo);
